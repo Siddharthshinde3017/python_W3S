038b8d4854ef66bbfa31e798d3a61d826a9a38de
Siddharthshinde3017 <117718492+Siddharthshinde3017@users.noreply.github.com>
2025-07-28 14:33:06 +0530

    some methods of string reversing added

--- rev_string.py
+++ rev_string.py
@@ -1,4 +1,29 @@
-string = input("Enter a string: ")  # Example input: "hello"
-def rev_str(string):
-    return string[::-1]
-print("Reversed string is:", rev_str(string))
+
+# reverse string using slicing method
+# string = input("Enter a string: ")  # Example input: "hello"
+# def rev_str(string):
+#     return string[::-1]
+# print("Reversed string is:", rev_str(string))
+
+
+# with for loop and logic
+# string = "sid"
+# lem = len(string)
+#
+# for i in range(lem-1,-1,-1):
+#     print(string[i], end=" ")
+
+# with function
+string= input("Enter the string")
+def rev(s):
+    lem = len(s)
+    for i in range(lem-1, -1,-1):
+        print(s[i], end=" ")
+print(rev(string))
+# print(f"the reverse steing is {rev()}")
+
+
+
+
+
+
