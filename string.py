9b4eb73d8f255a7f52d4065227d11f443c95c042
Siddharthshinde3017 <117718492+Siddharthshinde3017@users.noreply.github.com>
2025-07-31 00:20:28 +0530

    program of number reversing added

---
+++ string.py
@@ -0,0 +1,15 @@
+a = '''Like many other popular programming languages, strings in Python are arrays of unicode characters.
+
+However, Python does not have a character data type, a single character is simply a string with a length of 1.
+
+Square brackets can be used to access elements of the string.'''
+
+
+txt = "The best things in life are free!"
+# print('free' in txt)
+if "free" in txt:
+    print('yes free is present')
+
+
+name = "siddharth shinde"
+print(name[3:5])
