fd1f13efd48fb2a6951a0e6b9ae06bb178436032
Siddharthshinde3017 <117718492+Siddharthshinde3017@users.noreply.github.com>
2025-11-01 22:30:54 +0530

    new programs added

--- newone.py
+++ newone.py
@@ -1,2 +1,15 @@
-a = True + False
-print(a)
+# a = True + False
+# print(a)
+def decorator(func):
+    def wrapper():
+        print("Before function")
+        func()
+        print("After function")
+    return wrapper
+
+# @decorator
+# def greet():
+#     print("Hello!")
+
+
+# greet()
